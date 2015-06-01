46e7ff654bd84483a7b728ee36e45711ebe42e3e
vineethvarghese <vineeth.varghese@gmail.com>
2015-06-01 16:08:35 +1000

    Very rough

--- maestro-macros/src/main/scala/au/com/cba/omnia/maestro/macros/Inspect.scala
+++ maestro-macros/src/main/scala/au/com/cba/omnia/maestro/macros/Inspect.scala
@@ -23,6 +23,116 @@ import au.com.cba.omnia.humbug.HumbugThriftStruct
 object Inspect {
   val ProductField = """_(\d+)""".r
 
+  case class FieldDefinition[T, G] (
+    name: String,
+    index: Int,
+    type: T,
+    getter: G
+  )
+
+  def allFields[A <: ThriftStruct : c.WeakTypeTag](c: Context)(typ: c.universe.Type): List[FieldDefinition] = {
+    import c.universe.{Symbol => _, _}
+    /** Gets all the fields of a Thrift struct sorted in order of definition, for any type assumed to be ThriftStruct.*/
+    def fields(c: Context): List[(String, c.universe.MethodSymbol)] = {
+      import c.universe._
+
+      val typ = c.universe.weakTypeOf[A]
+      val productFieldRegex = """_(\d+)""".r
+
+      /** Gets all the `_1` style getters for a thrift struct in numerical order.*/
+      val methodSymbols =
+        typ.members.toList.map(member => (member, member.name.toString)).collect({
+          case (member, productFieldRegex(n)) => (member.asMethod, n.toInt)
+        }).sortBy(_._2).map(_._1)
+
+      val names =
+        if (typ <:< c.universe.weakTypeOf[HumbugThriftStruct]) {
+          // Get the fields in declaration order
+          typ.declarations.sorted.toList.collect {
+            case sym: TermSymbol if sym.isVar => sym.name.toString.trim
+          }
+        } else {
+          typ.typeSymbol.companionSymbol.typeSignature
+            .member(newTermName("apply")).asMethod.paramss.head.map(_.name.toString)
+        }
+
+      methodSymbols.zip(names)
+    }
+
+    val srcType       = c.universe.weakTypeOf[A]
+    val humbugTyp     = c.universe.weakTypeOf[HumbugThriftStruct]
+
+    val dstFields     = fields[A](c)
+    val expectedTypes = dstFields.map { case (f, n) => (n, f.returnType) }
+
+    //val in  = newTermName(c.fresh)
+
+    // def humbugGen(typ: Type, args: List[(String, Type)]): Tree = {
+    //   // https://issues.scala-lang.org/browse/SI-8425
+    //   //val out = newTermName(c.fresh) // Requires scala-2.11
+    //   val out = newTermName("out")
+
+    //   def mkInner(args: List[(String, Type)]): Tree = {
+    //     if (args.isEmpty) {
+    //       q"$out"
+    //     } else {
+    //       val (n, t) = args.head
+    //       val nn     = newTermName(n)
+    //       val ni     = Ident(nn)
+    //       val inner  = mkInner(args.tail)
+    //       q"""arbitrary[$t] flatMap { $ni: $t => $out.$nn = $ni; ..$inner }"""
+    //     }
+    //   }
+
+    //   val inner = mkInner(args)
+
+    //   q"""
+    //     val $out = new $typ
+    //     ..$inner
+    //   """
+    // }
+
+    // def scroogeGen(typ: Type, args: List[(String, Type)]): Tree = {
+    //   def mkNew(vals: List[c.Tree]) = {
+    //     val companion = typ.typeSymbol.companionSymbol
+    //     Apply(Select(Ident(companion), newTermName("apply")), vals)
+    //   }
+
+    //   def mkInner(args: List[(String, Type)], terms: List[Tree]): Tree = {
+    //     if (args.isEmpty) {
+    //       mkNew(terms)
+    //     } else {
+    //       val (n, t) = args.head
+    //       val nn     = Ident(newTermName(n))
+    //       val inner  = mkInner(args.tail, terms :+ nn)
+    //       q"""arbitrary[$t] flatMap { $nn: $t => ..$inner }"""
+    //     }
+    //   }
+
+    //   mkInner(args, List())
+    // }
+
+    // val body = srcType match {
+    //   case t if t <:< humbugTyp => humbugGen(srcType, expectedTypes)
+    //   case _                    => scroogeGen(srcType, expectedTypes)
+    // }
+
+    // val result = q"""
+    //   import org.scalacheck.Arbitrary
+    //   import org.scalacheck.Arbitrary.arbitrary
+    //   import org.scalacheck.Gen
+    //   Arbitrary[$srcType]($body)
+    // """
+
+    // c.Expr[Arbitrary[A]](result)
+  }
+
+  
+
+   
+
+  }
+
   /** Gets all the `_1` style getters and their number for a thrift struct in numerical order.*/
   def indexed[A <: ThriftStruct: c.WeakTypeTag](c: Context): List[(c.universe.MethodSymbol, Int)] = 
     indexedUnsafe(c)(c.universe.weakTypeOf[A])
